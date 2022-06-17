332fdc6010f8104fd5406660c5215bc316e9dba6
theoboldalex <theoboldalex@gmail.com>
2022-06-17 23:17:55 +0100

    Write message to floating window

--- lua/bad-habits/init.lua
+++ lua/bad-habits/init.lua
@@ -2,8 +2,8 @@ require "../messages"
 
 local M = {}
 
-local width = 50
-local height = 10
+local width = 75
+local height = 15
 local window_width = (vim.api.nvim_win_get_width(0) / 2) - (width / 2)
 local window_height = (vim.api.nvim_win_get_height(0) / 2) - (height / 2)
 local opts = {
@@ -15,11 +15,21 @@ local opts = {
     col = window_width,
 }
 
+local random_message_index = function()
+    math.randomseed(os.time())
+    return math.random(#MESSAGES)
+end
+
 M.show_warning = function()
-    for k, v in ipairs(MESSAGES) do
-        print(v)
-    end
     local buffer = vim.api.nvim_create_buf(false, true)
+    local message = MESSAGES[random_message_index()]
+
+    -- get the key that triggered the warning
+    --
+    -- get suggestion for alternative keystroke
+    --
+
+    vim.api.nvim_buf_set_lines(buffer, 0, 0, false, {message})
     vim.api.nvim_open_win(buffer, true, opts)
 end
 
--- lua/messages/init.lua
+++ lua/messages/init.lua
@@ -2,4 +2,5 @@ MESSAGES = {
     "Whoa Nellie!",
     "What yer playin' at soft lad?",
     "Now then, who's been a naughty boy?",
+    "Looks like somebody hasn't been practicing vimtutor!",
 }
