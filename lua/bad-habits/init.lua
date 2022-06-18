c61575d8246baf6d5cb0c3d2645b85ec932146f6
theoboldalex <theoboldalex@gmail.com>
2022-06-18 19:55:36 +0100

    Create function to return offsets table

--- lua/bad-habits/init.lua
+++ lua/bad-habits/init.lua
@@ -30,19 +30,34 @@ local fill_buffer = function()
     return filler
 end
 
+local get_text_offsets = function()
+    return {
+        start_row = 5,
+        end_row = 5,
+        start_col = 5,
+        end_col = 5
+    }
+end
+
 M.show_warning = function()
     local buffer = vim.api.nvim_create_buf(false, true)
     local buffer_content = fill_buffer()
     local message = MESSAGES[random_message_index()]
-
+    local message_offsets = get_text_offsets()
     -- get the key that triggered the warning
     --
     -- get suggestion for alternative keystroke
     --
 
-
     vim.api.nvim_buf_set_lines(buffer, 0, -1, false, buffer_content)
-    vim.api.nvim_buf_set_text(buffer, 5, 5, 5, 5, {message})
+    vim.api.nvim_buf_set_text(
+        buffer, 
+        message_offsets["start_row"],
+        message_offsets["start_col"],
+        message_offsets["end_row"],
+        message_offsets["end_col"],
+        {message}
+    )
     vim.api.nvim_open_win(buffer, true, opts)
 end
 
