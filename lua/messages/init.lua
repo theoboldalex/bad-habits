beab6febdd4d7af0b06706eb1d8d7788874167e0
theoboldalex <theoboldalex@gmail.com>
2022-06-22 18:48:02 +0100

    update messgaes

--- lua/messages/init.lua
+++ lua/messages/init.lua
@@ -3,4 +3,7 @@ MESSAGES = {
     "What yer playin' at soft lad?",
     "Now then, who's been a naughty boy?",
     "Looks like somebody hasn't been practicing vimtutor!",
+    "Tut tut...",
+    "Don't be a noob",
+    "You're better than this"
 }
