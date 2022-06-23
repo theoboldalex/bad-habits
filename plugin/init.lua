e9314eb5e21eb3b404787e2af40ac6230cce5b15
theoboldalex <theoboldalex@gmail.com>
2022-06-23 19:49:35 +0100

    Comment

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,6 +1,7 @@
 vim.cmd([[command! NaughtyBoy :lua require"bad-habits".show_warning()]])
 
 -- Arrow keys
+-- TODO: Pass Key as arg to NaughtyBoy cmd to use in warninig message.
 vim.api.nvim_set_keymap("n", "<Up>", "<cmd>NaughtyBoy<cr>", {})
 vim.api.nvim_set_keymap("n", "<Down>", "<cmd>NaughtyBoy<cr>", {})
 vim.api.nvim_set_keymap("n", "<Left>", "<cmd>NaughtyBoy<cr>", {})
