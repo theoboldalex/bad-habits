bc00fb519b91191bbd8b355656eee89108301a49
theoboldalex <theoboldalex@gmail.com>
2022-06-23 19:50:49 +0100

    Sketch passing key to command

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,6 +2,8 @@ vim.cmd([[command! NaughtyBoy :lua require"bad-habits".show_warning()]])
 
 -- Arrow keys
 -- TODO: Pass Key as arg to NaughtyBoy cmd to use in warninig message.
+-- Something along the lines of...
+-- vimi.api.nvim_set_keymap("n", "<Up>", "<cmd>NaughtyBoy <Up><cr>", {})
 vim.api.nvim_set_keymap("n", "<Up>", "<cmd>NaughtyBoy<cr>", {})
 vim.api.nvim_set_keymap("n", "<Down>", "<cmd>NaughtyBoy<cr>", {})
 vim.api.nvim_set_keymap("n", "<Left>", "<cmd>NaughtyBoy<cr>", {})
