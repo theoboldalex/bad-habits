8982581b1bad1d03bfe26a80b85aafcbb389f75d
theoboldalex <theoboldalex@gmail.com>
2022-06-30 15:02:59 +0100

    Update message wording

--- lua/messages/init.lua
+++ lua/messages/init.lua
@@ -2,7 +2,7 @@ MESSAGES = {
     "Whoa Nellie!",
     "What yer playin' at soft lad?",
     "Now then, who's been a naughty boy?",
-    "Looks like somebody hasn't been practicing vimtutor!",
+    "You better start practicing vimtutor!",
     "Tut tut...",
     "Don't be a noob",
     "You're better than this"
